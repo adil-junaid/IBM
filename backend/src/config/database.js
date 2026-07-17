2bc8d3d90ef9178104357936c69118bcb4af6d80
Adil Junaid <adiljunaidayath@gmail.com>
2026-07-17 21:21:29 +0530

    Working MongoDB Atlas integration

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,14 +2,14 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    console.log("Connecting...");
+    await mongoose.connect(process.env.MONGO_URI);
 
-    await mongoose.connect(process.env.MONGODB_URI);
-
-    console.log("✅ MongoDB Connected Successfully");
-  } catch (error) {
-    console.error(error);
-  }
+    console.log("✅ MongoDB connected successfully");
+  } catch (err) {
+  console.error("❌ MongoDB connection failed");
+  console.error(err);
+  process.exit(1);
+}
 };
 
 module.exports = connectDB;
